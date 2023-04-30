49f98c03202d2e10900e5d670836759b4871f44a
Rodrigo Silveira <gsrodriigo@gmail.com>
2023-04-30 01:56:51 -0300

    two-sum golang resolution

---
+++ two-sum/golang.go
@@ -0,0 +1,17 @@
+package two_sum
+
+func TwoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+
+		if j, ok := hashMap[complement]; ok {
+			return []int{i, j}
+		}
+
+		hashMap[num] = i
+	}
+
+	return nil
+}
