dcd3e29158c42316f6c59cd2a90e17c6fb2a2af7
Rodrigo Silveira <gsrodriigo@gmail.com>
2023-04-30 02:50:45 -0300

    longest-common-prefix javascript update

--- longest-common-prefix/javascript.js
+++ longest-common-prefix/javascript.js
@@ -3,10 +3,6 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (!strs.length) {
-        return '';
-    }
-
     let prefix = strs[0];
 
     for (let i = 1; i < strs.length; i++) {
@@ -19,5 +15,5 @@ var longestCommonPrefix = function(strs) {
         }
     }
 
-    return prefix;
+    return prefix ?? '';
 };
